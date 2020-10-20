9f6cc26a63220777f0d1620edd15d27771f21561
Chinmay Gawde <gawdechinmay007@gmail.com>
2020-10-20 17:35:55 +0530

    Post image and Log in done

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ var app = express();
 var bodyParser = require("body-parser");
 const { ObjectID } = require("mongodb");
 
+var formidable = require("formidable");
+var fs = require("fs");
+var session = require("express-session");
+
+app.use(session({
+    key: "admin",
+    secret: "string"
+
+}));
+
 app.use("/static", express.static(__dirname + "/static"));
 app.set("view engine", "ejs");
 
@@ -13,6 +23,21 @@ var MongoClient = require("mongodb").MongoClient;
 MongoClient.connect("mongodb://localhost:27017", {useNewUrlParser:true},
 function(error,client){
     var blog =client.db("blog");
+
+    if(blog.collection("admin")){
+
+    }else{
+
+        blog.createCollection("admin", function(err, res){
+            if(err) throw err;
+            console.log("Admin collection created");
+        });
+
+    }
+
+    
+    
+    
     console.log("DB connected");
 
     app.get("/", function(req,res){
@@ -29,17 +54,54 @@ function(error,client){
     });
 
     app.get("/admin/dashboard", function(req,res){
-        res.render("admin/dashboard");
+        if(req.session.admin){
+            res.render("admin/dashboard");
+        }
+        else{
+            res.redirect("admin");
+        }
+        
+    });
+
+    app.get("/admin", function(req,res){
+        res.render("admin/login");
     });
 
     app.get("/admin/posts", function(req,res){
-        res.render("admin/posts");
+        res.render("admin/post");
     });
 
     app.post("/do-post", function(req,res){
         blog.collection("posts").insertOne(req.body, function(error, document){
             res.send("Posted successfully");
         });
+    });
+
+    app.post("/do-upload-image", function(req,res){
+        var formData = new formidable.IncomingForm();
+        formData.parse(req, function(error, fields, files){
+            var oldPath = files.file.path;
+            var newPath = "static/images/" + files.file.name;
+
+            fs.rename(oldPath, newPath, function(err){
+                res.send("/" + newPath);
+            });
+        } );
+
+    } );
+
+    app.post("/do-admin-login", function(req, res){
+        blog.collection("admin").findOne({"email": req.body.email, "password": req.body.password}, function(error, admin){
+            if(admin != ""){
+                req.session.admin = admin;
+            }
+            res.send(admin);
+        });
+    });
+
+    app.get("/do-logout", function(req,res){
+        req.session.destroy();
+        res.redirect("/admin");
     });
 
     
